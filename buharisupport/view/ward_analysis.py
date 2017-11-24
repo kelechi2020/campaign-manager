0b3df09a93860b028d78b3655d8542b8e8fbe438
kelechi2020 <egbosikelechi@gmail.com>
2017-11-23 16:44:31 -0800

    get query count

--- buharisupport/view/ward_analysis.py
+++ buharisupport/view/ward_analysis.py
@@ -5,8 +5,6 @@ from buharisupport.models import LocalGovernment, Members, Ward
 
 def page(request, localgov):
     ward = Ward.objects.filter(local_government__name=localgov)
-    ward_number = [Members.objects.filter(ward=count).count() for count in ward]
-    from pprint import pprint
-    pprint(ward_number)
+    ward_number = [Members.objects.filter(ward=count.ward_name).count() for count in ward]
     new = zip(ward,ward_number)
     return render(request, 'ward_analysis.html',{'new':new, 'state':localgov} )
